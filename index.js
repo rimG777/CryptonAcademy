89f282f9cbf262bbe0c8bdf6fa27c1f7abe8c91b
rimG777 <101937769+rimG777@users.noreply.github.com>
2022-03-19 15:59:36 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+function TestSquare(symbol, rate, used){
+  this.symbol = symbol;
+  this.rate = rate;
+  this.used = user;
+}
+
+print('hello world')
+console.log('hello world')
+
+
+
+// Helper function to print to the screen. 
+function print(line) {
+  const appDiv = document.getElementById('app');
+  const div = document.createElement('div');
+  div.innerHTML = line;
+  appDiv.appendChild(div)
+}
